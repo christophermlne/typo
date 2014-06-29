7eaf1060d9176a18c7bd0aec1ef693cb4912f1a4
Christopher Milne <christopher.milne@gmail.com>
2014-06-29 11:42:16 -0400

    Merge article feature progress

--- features/step_definitions/merge_article_steps.rb
+++ features/step_definitions/merge_article_steps.rb
@@ -1,7 +1,5 @@
 Given(/^the following articles exist$/) do |table|
   # table is a Cucumber::Ast::Table
-  # pending # express the regexp above with the code you wish you had
- 
   @data = table.hashes
   @data.each do |article|
 	@article = Article.new(:title => article['Title'], :author => article['Author'], :body => article['Body'])
